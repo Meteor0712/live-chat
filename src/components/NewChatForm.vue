f92e5d4f3490238feda9707cb01b120e96e701e1
Meteor <refined90926@gmail.com>
2022-03-09 21:37:22 +0800

    building & deploying(firebase hosting)

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form>
+      <textarea
+        placeholder="Type a message and hit enter to end..."
+        v-model="message"
+        @keypress.enter.prevent="handleSubmit">
+      </textarea>
+      <div class="error">{{ error }}</div>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import getUser from '../composables/getUser'
+import useCollection from '../composables/useCollection'
+import { timestamp } from '../firebase/config'
+export default {
+    setup() {
+        const { user } = getUser() 
+        const { addDoc, error } = useCollection('messages')
+
+        const message = ref('')
+
+        const handleSubmit = async () => {
+            const chat = {
+                name: user.value.displayName,
+                message: message.value,
+                createdAt: timestamp()
+            }
+
+            await addDoc(chat)
+            if (!error.value) {
+                message.value = ''
+            }
+            
+        }
+
+        return { handleSubmit, message, error }
+    }
+}
+// scoped是為了避免目前元件的style會汙染子元件
+</script>
+
+<style scoped>
+    form {
+        margin: 10px;
+    }
+    textarea {
+        width: 100%;
+        max-width: 100%;
+        margin-bottom: 6px;
+        padding: 10px;
+        box-sizing: border-box;
+        border: 0;
+        border-radius: 20px;
+        font-family: inherit;
+        outline: none;
+    }
+</style>
